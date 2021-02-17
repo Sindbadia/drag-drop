79db99cd0595e4a7194f994b4472ff02359059ca
Sindbadia <52345388+Sindbadia@users.noreply.github.com>
2021-02-17 15:06:25 +0200

    FIX(form): Add input validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,39 @@
+// Validation
+interface Validatable {
+	value: string | number
+	required?: boolean
+	minLength?: number
+	maxLength?: number
+	min?: number
+	max?: number
+}
+
+function validate(validatableInput: Validatable): boolean {
+	let isValid = true
+	if (validatableInput.required) {
+		isValid = !!validatableInput.value.toString().trim() && isValid
+	}
+
+	if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+		isValid = validatableInput.value.length >= validatableInput.minLength && isValid
+	}
+
+	if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+		isValid = validatableInput.value.length <= validatableInput.maxLength && isValid
+	}
+
+	if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+		isValid = validatableInput.value >= validatableInput.min && isValid
+	}
+
+	if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+		isValid = validatableInput.value <= validatableInput.max && isValid
+	}
+
+	return isValid
+}
+
+// autobind decorator
 function autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
 	const originalMethod = descriptor.value
 	const adjDescriptor = {
@@ -27,17 +63,63 @@ class ProjectInput {
 		this.formEl.id = 'user-input'
 
 		this.titleInputEl = this.formEl.querySelector('#title')! as HTMLInputElement
-		this.descInputEl = this.formEl.querySelector('#descripion')! as HTMLInputElement
+		this.descInputEl = this.formEl.querySelector('#description')! as HTMLInputElement
 		this.peopleInputEl = this.formEl.querySelector('#people')! as HTMLInputElement
 
 		this.configure()
 		this.attach()
 	}
 
+	private gatherUserInput(): [string, string, number] | void {
+		const enteredTitle = this.titleInputEl.value
+		const enteredDesc = this.descInputEl.value
+		const enteredPeople = this.peopleInputEl.value
+
+		const titleValidatable: Validatable = {
+			value: enteredTitle,
+			required: true,
+		}
+		const descValidatable: Validatable = {
+			value: enteredDesc,
+			required: true,
+			minLength: 5,
+		}
+		const peopleValidatable: Validatable = {
+			value: Number(enteredPeople),
+			required: true,
+			min: 1,
+			max: 5,
+		}
+
+		if (
+			!validate(titleValidatable) ||
+			!validate(descValidatable) ||
+			!validate(peopleValidatable)
+		) {
+			alert('Invalid input please try again!')
+			return
+		} else {
+			return [enteredTitle, enteredDesc, Number(enteredPeople)]
+		}
+	}
+
+	private clearInputs() {
+		this.titleInputEl.value = ''
+		this.descInputEl.value = ''
+		this.peopleInputEl.value = ''
+		this.titleInputEl.focus()
+	}
+
 	@autobind
 	private submitHandler(event: Event) {
 		event.preventDefault()
-		console.log(this.titleInputEl.value)
+		const userInput = this.gatherUserInput()
+
+		if (Array.isArray(userInput)) {
+			const [title, desc, people] = userInput
+			console.log(title, desc, people)
+			this.clearInputs()
+		}
 	}
 
 	private configure() {
